1167bfb12c01e4b1802b005bf9838e77353b89c0
Maribelle Leong <maribelle.leong@gmail.com>
2026-08-13 13:39:09 -0400

    faded color, option 1

--- script.js
+++ script.js
@@ -38,3 +38,25 @@ function setupNavigation() {
 }
 
 loadNavbar();
+
+/*
+    // blue glow — top right 
+    radial-gradient(
+      450px 380px at 95% 4%,
+      rgba(35, 85, 180, 0.09),
+      transparent 100%
+    ),
+    // blue glow — middle left
+    radial-gradient(
+        420px 500px at -5% 48%,
+        rgba(35, 85, 180, 0.055),
+        transparent 100%
+      ),
+    //blue glow — lower right 
+    radial-gradient(
+        420px 480px at 105% 82%,
+        rgba(35, 85, 180, 0.05),
+        transparent 100%
+      );
+
+*/
